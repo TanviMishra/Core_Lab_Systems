3a792b34d1afd4ff721b7529af83e334c353b6e1
TanviMishra <60146957+TanviMishra@users.noreply.github.com>
2021-11-01 15:14:36 -0400

    Add files via upload

---
+++ CAS Prototype/scripts/main.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sidenav").style.width = "25vw";
+    //document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
